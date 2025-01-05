dba19d72f3302ef0a0c645c525bec5b283030787
Ian Contijoch <ian.contijoch@gmail.com>
2025-01-05 05:08:38 -0500

    day16-parts1+2

---
+++ day16/part1.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+
+    counts = {
+        'children': 3,
+        'cats': 7,
+        'samoyeds': 2,
+        'pomeranians': 3,
+        'akitas': 0,
+        'vizslas': 0,
+        'goldfish': 5,
+        'trees': 3,
+        'cars': 2,
+        'perfumes': 1,
+    }
+
+    def check(s: str, counts: dict[str, int]) -> bool:
+        for chunk in s.split(', '):
+            k, v = chunk.split(': ')
+            if counts[k] != int(v):
+                return False
+        return True
+
+    lines = s.splitlines()
+    for line in lines:
+        id_num = int(line.split()[1][:-1])
+        items_s = ' '.join(line.split()[2:])
+        if check(items_s, counts):
+            return id_num
+    return 0
+
+
+INPUT_S = '''\
+Sue 1: children: 1, cars: 8, vizslas: 7
+Sue 2: akitas: 10, perfumes: 10, children: 5
+Sue 3: cars: 5, pomeranians: 4, vizslas: 1
+Sue 213: children: 3, goldfish: 5, vizslas: 0
+'''
+EXPECTED = 213
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
