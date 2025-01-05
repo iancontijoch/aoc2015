96c2e66283f921c6aa4fb4323787a17134dad456
Ian Contijoch <ian.contijoch@gmail.com>
2025-01-05 10:36:52 -0500

    day18-parts1+2

---
+++ day18/part1.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    coords = support.parse_coords(s)
+    # support.print_coords(coords)
+    for _ in range(100):
+        on = {pos for pos, c in coords.items() if c == '#'}
+        new_on = set()
+        new_off = set()
+
+        for pos in coords:
+            num_on = len(
+                {adj for adj in support.adjacent_8(*pos) if adj in on},
+            )
+            if pos in on and num_on not in (2, 3):
+                new_off.add(pos)
+            if pos not in on and num_on == 3:
+                new_on.add(pos)
+
+        coords.update({pos: '#' for pos in new_on})
+        coords.update({pos: '.' for pos in new_off})
+        # print()
+        # support.print_coords(coords)
+
+    return len({pos for pos, c in coords.items() if c == '#'})
+
+
+INPUT_S = '''\
+.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
